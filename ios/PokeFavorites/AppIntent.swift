8589052b97adc7a1fcc12f047c7e18a183b7a433
Danfelogar <danypolo18@hotmail.com>
2024-03-07 01:57:17 -0500

    feat: fixing widget with configuration un build phaces drag and drop

---
+++ ios/PokeFavorites/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  PokeFavorites
+//
+//  Created by Daniel Felipe on 6/03/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ios/PokeFavorites/PokeFavoritesBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PokeFavoritesBundle.swift
+//  PokeFavorites
+//
+//  Created by Daniel Felipe on 6/03/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PokeFavoritesBundle: WidgetBundle {
+    var body: some Widget {
+        PokeFavorites()
+    }
+}
